8bf5cb1a8565910f31547a25b019ba03ab3aea8c
Sundeep Charan Ramkumar <sunguru98@yahoo.co.in>
2018-11-11 12:24:55 +0530

    Init commit

--- appone/models.py
+++ appone/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Topic(models.Model):
+    topicName = models.CharField(max_length=250,unique=True)
+
+    def __str__(self):
+        return self.topicName
+
+class Webpage(models.Model):
+    topic = models.ForeignKey('Topic',on_delete=models.CASCADE)
+    webpageName = models.CharField(max_length=250,unique=True)
+    webpageUrl = models.URLField()
+
+    def __str__(self):
+        return self.webpageName
+
+class AccessRecord(models.Model):
+    webpage = models.ForeignKey('Webpage',on_delete=models.CASCADE)
+    accessedDate = models.DateField()
+
+    def __str__(self):
+        return str(self.accessedDate)
